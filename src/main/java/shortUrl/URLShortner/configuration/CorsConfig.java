5bbaf6f69bda106032a5f4604e0abb732fb5331c
Shubhankar Sharma <shubhankarsharma22@gmail.com>
2024-08-19 14:01:27 +0530

    Backend done completely

--- src/main/java/shortUrl/URLShortner/configuration/CorsConfig.java
+++ src/main/java/shortUrl/URLShortner/configuration/CorsConfig.java
@@ -1,2 +1,17 @@
-package shortUrl.URLShortner.configuration;public class CorsConfig {
+package shortUrl.URLShortner.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Corrected path pattern
+                .allowedOrigins("http://localhost:5173") // Frontend origin
+                .allowedMethods("GET", "POST", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
